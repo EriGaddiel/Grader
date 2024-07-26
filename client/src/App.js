adbbe0089a10b210a546545ece3f673c92e0622a
Darael Eri Gaddiel <daraeleri@gmail.com>
2024-07-26 13:35:38 +0100

    Adding the navbar component to the routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,32 @@
 import React from 'react'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom'
 import Home from './Pages/Home'
 import SignUp from './Pages/SignUp'
 import Login from './Pages/Login'
+import Navbar from './Components/Navbar'
 
 function App() {
+
   return (
-    <BrowserRouter>
+    <Router>
+      <Main/>
+    </Router>
+  )
+}
+
+
+const Main = () => {
+  const path = useLocation()
+  const showNavbar = !['/login', '/sign-up'].includes(path.pathname)
+  return (
+    <>
+      {showNavbar && <Navbar />}
       <Routes>
         <Route path="/" element={<Home/>} />
         <Route path="/sign-up" element={<SignUp/>} />
         <Route path="/login" element={<Login/>} />
       </Routes>
-    </BrowserRouter>
+    </>
   )
 }
 
