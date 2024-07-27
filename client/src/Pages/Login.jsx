9e898cce2338323f32035e457c42718e8b868f88
Darael Eri Gaddiel <daraeleri@gmail.com>
2024-07-27 10:48:52 +0100

    Login page Ui

--- client/src/Pages/Login.jsx
+++ client/src/Pages/Login.jsx
@@ -1,11 +1,63 @@
 import React from 'react'
+import {Link} from 'react-router-dom' 
+import { motion } from 'framer-motion'
+import {Label, TextInput, Button, Spinner} from 'flowbite-react'
 
 function Login() {
   return (
-    <div>
-      Login
+    <div className=" min-h-screen flex items-center justify-center">
+      <div className="flex p-3 max-w-3xl mx-auto flex-col md:flex-row md:items-center gap-5"> 
+
+        {/* To the Left */}
+        <motion.div 
+          className='flex-1'
+          initial={{ opacity: 0, x: -100 }}
+          animate={{ opacity: 1, x: 0 }}
+          exit={{ opacity: 0, x: -100 }}
+          transition={{ duration: 1 }}
+        >
+          <h1 className="text-5xl text-blue-700 font-bold">Grader</h1>
+
+          <p className="text-sm mt-5 text-[#868AA5]">
+           Log In to Continue Your Journey of Connection and Collaboration!
+          </p>
+        </motion.div>
+
+        {/* To Right */}
+        <motion.div 
+          className='flex-1 dark:text-white'
+          initial={{ opacity: 0, x: 100 }}
+          animate={{ opacity: 1, x: 0 }}
+          exit={{ opacity: 0, x: 100 }}
+          transition={{ duration: 1 }}
+          >
+          <form className='flex flex-col gap-4'>
+              <div>
+                <Label value='Email' />
+                <TextInput type='email' placeholder='Enter your email' id='email' className='w-full'/>
+              </div>
+
+              <div>
+                <Label value='Password' />
+                <TextInput type='password' placeholder='Enter your password' id='password' className='w-full'/>
+              </div>
+
+              <Button gradientDuoTone="purpleToBlue" type='submit'>
+                  Login
+            </Button>
+          </form>
+
+          <div className="flex gap-2 text-sm mt-5">
+            <span>Haven't an account?</span>
+            <Link to='/sign-up' className='text-blue-500'>
+              Sign In
+            </Link>
+          </div>
+
+        </motion.div>
+      </div>
     </div>
-  )
+  );
 }
 
 export default Login
--- client/src/Pages/SignUp.jsx
+++ client/src/Pages/SignUp.jsx
@@ -1,68 +1,69 @@
-import React from 'react'
-import {Link} from 'react-router-dom' 
-import { motion } from 'framer-motion'
-import {Label, TextInput, Button, Spinner} from 'flowbite-react'
+  import React, { useEffect, useState} from 'react'
+  import {Link} from 'react-router-dom' 
+  import { motion } from 'framer-motion'
+  import {Label, TextInput, Button, Spinner} from 'flowbite-react'
 
-function SignUp() {
-  return (
-    <div className=" min-h-screen flex items-center justify-center">
-      <div className="flex p-3 max-w-3xl mx-auto flex-col md:flex-row md:items-center gap-5"> 
 
-        {/* To the Left */}
-        <motion.div 
-          className='flex-1'
-          initial={{ opacity: 0, x: -100 }}
-          animate={{ opacity: 1, x: 0 }}
-          exit={{ opacity: 0, x: -100 }}
-          transition={{ duration: 1 }}
-        >
-          <h1 className="text-5xl text-blue-700 font-bold">Grader</h1>
+  function SignUp() {
+    return (
+      <div className=" min-h-screen flex items-center justify-center">
+        <div className="flex p-3 max-w-3xl mx-auto flex-col md:flex-row md:items-center gap-5"> 
 
-          <p className="text-sm mt-5 text-[#868AA5]">
-            Enhancing university interactions. Connect, collaborate, excel together with Grader
-          </p>
-        </motion.div>
-
-        {/* To Right */}
-        <motion.div 
-          className='flex-1'
-          initial={{ opacity: 0, x: 100 }}
-          animate={{ opacity: 1, x: 0 }}
-          exit={{ opacity: 0, x: 100 }}
-          transition={{ duration: 1 }}
+          {/* To the Left */}
+          <motion.div 
+            className='flex-1'
+            initial={{ opacity: 0, x: -100 }}
+            animate={{ opacity: 1, x: 0 }}
+            exit={{ opacity: 0, x: -100 }}
+            transition={{ duration: 1 }}
           >
-          <form className='flex flex-col gap-4'>
-              <div>
-                <Label value='Your username' />
-                <TextInput type='text' placeholder='Enter your username' id='username' className='w-full'/>
-              </div>
+            <h1 className="text-5xl text-blue-700 font-bold">Grader</h1>
 
-              <div>
-                <Label value='Email' />
-                <TextInput type='email' placeholder='Enter your email' id='email' className='w-full'/>
-              </div>
+            <p className="text-sm mt-5 text-[#868AA5]">
+              Enhancing university interactions. Connect, collaborate, excel together with Grader
+            </p>
+          </motion.div>
 
-              <div>
-                <Label value='Password' />
-                <TextInput type='password' placeholder='Enter your password' id='password' className='w-full'/>
-              </div>
+          {/* To Right */}
+          <motion.div 
+            className='flex-1 dark:text-white'
+            initial={{ opacity: 0, x: 100 }}
+            animate={{ opacity: 1, x: 0 }}
+            exit={{ opacity: 0, x: 100 }}
+            transition={{ duration: 1 }}
+            >
+            <form className='flex flex-col gap-4'>
+                <div>
+                  <Label value='Your username' />
+                  <TextInput type='text' placeholder='Enter your username' id='username' className='w-full'/>
+                </div>
 
-              <Button gradientDuoTone="purpleToBlue" type='submit'>
-                Sign Up
-            </Button>
-          </form>
+                <div>
+                  <Label value='Email' />
+                  <TextInput type='email' placeholder='Enter your email' id='email' className='w-full'/>
+                </div>
 
-          <div className="flex gap-2 text-sm mt-5">
-            <span>Have an account?</span>
-            <Link to='/login' className='text-blue-500'>
-              Sign In
-            </Link>
-          </div>
+                <div>
+                  <Label value='Password' />
+                  <TextInput type='password' placeholder='Enter your password' id='password' className='w-full'/>
+                </div>
 
-        </motion.div>
+                <Button gradientDuoTone="purpleToBlue" type='submit'>
+                  Sign Up
+              </Button>
+            </form>
+
+            <div className="flex gap-2 text-sm mt-5">
+              <span>Have an account?</span>
+              <Link to='/login' className='text-blue-500'>
+                Sign In
+              </Link>
+            </div>
+
+          </motion.div>
+        </div>
       </div>
-    </div>
-  );
-}
+    );
+  }
 
-export default SignUp
+  export default SignUp
