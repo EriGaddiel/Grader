68ac2156de9d850e16dad9a7cc3d7fc8bd30dda3
Darael Eri Gaddiel <daraeleri@gmail.com>
2024-08-03 11:09:12 +0100

    Backend controller and route for login

--- server/Controllers/auth.controller.js
+++ server/Controllers/auth.controller.js
@@ -2,6 +2,9 @@ import bcryptjs from 'bcryptjs'
 import User from '../Models/user.model.js'
 import { errorHandler } from '../utils/error.js'
 
+import jwt from 'jsonwebtoken'
+
+
 export const signup = async (req, res, next) => {
     
     const {username, email, password} = req.body
@@ -42,3 +45,35 @@ export const signup = async (req, res, next) => {
     }
 
 }
+
+
+
+export const login = async (req, res, next) =>{
+    const {email, password} = req.body
+
+    if(!email || !password || email === '' || password === ''){
+        next(errorHandler(400, 'All fields are required'))
+    }
+
+    try {
+        const validUser = await User.findOne({email})
+        if(!validUser){
+            return next(errorHandler(404,'Invalid email or password'))
+        }
+ 
+        const validPassword = bcryptjs.compareSync(password, validUser.password)
+
+        if (!validPassword) {
+            return next(errorHandler(404,'Invalid email or password'))
+        }
+
+        const token = jwt.sign({userId: validUser._id},process.env.JWT_SECRET)
+
+        const {password: pass, ...rest} = validUser._doc
+
+        res.status(200).cookie('Access_token', token, {httpOnly: true}).json(rest)
+
+    } catch (error) {
+        next(error)
+    }
+} 
